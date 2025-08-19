2b6a7778f469827af7c6a687bf6d902dd85182a8
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-08-19 08:44:05 +0300

    feat(#53411499): добавил данные для CICD

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+        config: {
+            provider: "strapi-provider-minio-brokstock",
+            providerOptions: {
+                accessKey: env("MINIO_ACCESS_KEY"),
+                secretKey: env("MINIO_SECRET_KEY"),
+                bucket: env("MINIO_BUCKET"),
+                endPoint: env("MINIO_ENDPOINT"),
+                host: env("MINIO_HOST"),
+                port: env("MINIO_PORT"),
+                useSSL: env("MINIO_USE_SSL"),
+            },
+        },
+    }
+});
